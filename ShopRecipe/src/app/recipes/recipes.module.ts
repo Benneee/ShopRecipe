75e490f2cfcd658e09e7105ea2b1f5ab598993e2
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-04-10 15:17:43 +0100

    Created more feature modules

--- ShopRecipe/src/app/recipes/recipes.module.ts
+++ ShopRecipe/src/app/recipes/recipes.module.ts
@@ -7,8 +7,8 @@ import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
-import { DropdownDirective } from "../shared/dropdown.directive";
 import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "../shared/shared.module";
 @NgModule({
   declarations: [
     RecipesComponent,
@@ -16,10 +16,14 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
     RecipeListComponent,
     RecipeEditComponent,
     RecipeDetailComponent,
-    RecipeItemComponent,
-    DropdownDirective
+    RecipeItemComponent
   ],
-  imports: [ReactiveFormsModule, RecipesRoutingModule, CommonModule]
+  imports: [
+    ReactiveFormsModule,
+    RecipesRoutingModule,
+    CommonModule,
+    SharedModule
+  ]
 })
 export class RecipesModule {}
 
