ec07c424a2ad29e6ce676934c13dea0703edb1f8
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-04-19 05:26:34 +0100

    Updated server.js file

--- ShopRecipe/server.js
+++ ShopRecipe/server.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // app.use(express.static("/dist"));
 
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
   // res.sendFile("/dist/shoprecipe/index.html");
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
