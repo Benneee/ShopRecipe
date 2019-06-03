cfc6567a28ec5e74434924f394b2d01ed7d9d1d5
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-06-03 05:03:31 +0100

    Enhanced analytics with events and actions

--- ShopRecipe/src/app/auth/signin/signin.component.ts
+++ ShopRecipe/src/app/auth/signin/signin.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
-import { AuthService } from "../auth.service";
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+declare var gtag;
 
 @Component({
-  selector: "app-signin",
-  templateUrl: "./signin.component.html",
-  styleUrls: ["./signin.component.css"]
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
   constructor(private authService: AuthService) {}
 
   ngOnInit() {}
+
+  sendEvent() {
+    gtag('event', 'log_in', {
+      event_category: 'authentication',
+      event_label: 'user signed in',
+      event_action: 'user signed in',
+      event_value: 1
+    });
+  }
 
   onSignin(form: NgForm) {
     const email = form.value.email;
--- ShopRecipe/src/app/auth/signup/signup.component.ts
+++ ShopRecipe/src/app/auth/signup/signup.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
-import { AuthService } from "../auth.service";
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+declare var gtag;
 
 @Component({
-  selector: "app-signup",
-  templateUrl: "./signup.component.html",
-  styleUrls: ["./signup.component.css"]
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
   constructor(private authService: AuthService) {}
 
   ngOnInit() {}
+
+  sendEvent() {
+    gtag('event', 'sign up', {
+      event_category: 'authentication',
+      event_label: 'user signed up',
+      event_action: 'user signed up',
+      event_value: 0
+    });
+  }
 
   onSignup(form: NgForm) {
     const email = form.value.email;
